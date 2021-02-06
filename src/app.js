e44ad4679c2a6ba99cb4156ff4ae9132147874e1
devDinoFukuda <dev.dinofukuda@gmail.com>
2021-02-06 18:53:28 -0300

    Entrega do Desafio

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { v4: uuid, validate: isUuid } = require('uuid');
+const { v4: uuid, validate: isUuid } = require('uuid');
 
 const app = express();
 
@@ -11,23 +11,71 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
-});
+  
+  
+  return response.json(repositories)
+  
+}); //ok
 
 app.post("/repositories", (request, response) => {
-  // TODO
-});
+  const { title, url, techs } = request.body;
+
+  const repository = { id: uuid(), title, url, techs, likes: 0 };
+  repositories.push(repository);
+
+  return response.json(repository)
+}); //ok
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
-});
+  
+  const { id } = request.params;
+  const { title, url, techs } = request.body;
+
+  const repositoriesId = repositories.findIndex(repository => repository.id === id);
+  if (repositoriesId <0 ) {
+    return response.status(400).json({error: 'Invalid Id.'})
+  }
+
+  const repository = { id, title, url, techs,
+    //puxa o número de likes sem alterar
+    likes : repositories[repositoriesId].likes  
+  };
+  repositories[repositoriesId] = repository;
+
+  return response.json(repository);
+
+
+}); //ok
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
-});
+  const { id } = request.params;
+  const repositoriesId = repositories.findIndex(
+    repository => repository.id === id);
+
+    if(repositoriesId < 0) {
+      return response.status(400).json({error: 'Invalid Id'});
+    }
+
+    repositories.splice(repositoriesId, 1);
+
+    return response.status(204).send()
+   
+}); //ok
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
-});
+  const { id } = request.params;
+  
+  const repositoriesId = repositories.findIndex(
+    repository => repository.id === id
+    );
+
+    if (repositoriesId === -1 ) {
+      return response.status(400).json({error: 'Invalid Id.'})
+    }
+
+      repositories[repositoriesId].likes++;
+      return response.json(repositories[repositoriesId]);
+
+}); //ok
 
 module.exports = app;
